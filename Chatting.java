c06f48c9322c5e10a7aab327ff80de531288dc8f
ventania1680 <44992800+ventania1680@users.noreply.github.com>
2019-06-18 22:48:01 +0900

    업데이트2
    서버로 이미지 전송

--- Chatting.java
+++ Chatting.java
@@ -150,6 +150,185 @@ public class Chatting extends AppCompatActivity {
 
     }
 
+
+    Thread connect_and_check = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                setSocket(ip, port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String line = "";
+            adapter = new ChattingAdapter();
+
+            while (true) {
+                try {
+                    line = networkReader.readLine().toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                globalLine = line;
+
+                if(globalLine.split("@")[1].equals("image_send_server_to_client"))
+                {
+                    String temp_name = globalLine.split("@")[2];
+                    String file_name = temp_name.split("\\.")[0] + "_" + file_num + "." + temp_name.split("\\.")[1];
+                    System.out.println(file_name);
+                    //String file_name = temp_name;
+
+                    String file_path = Environment.getExternalStorageDirectory() + "/" + file_name;
+                    File file = new File(file_path);
+                    FileOutputStream output = null;
+
+                    byte[] buf = new byte[1024];
+                    int total_size = 0;
+                    int max_size = Integer.parseInt(globalLine.split("@")[3]);
+                    int recv_size = 0;
+                    try {
+                        output = new FileOutputStream(file);
+                        InputStream is = socket.getInputStream();
+                        DataInputStream dis = new DataInputStream(is);
+
+                        while ((recv_size = dis.read(buf)) != -1) {
+                            total_size += recv_size;
+                            output.write(buf, 0, recv_size);
+                            output.flush();
+                            if (total_size >= max_size) {
+                                output.close();
+                                break;
+                            }
+                        }
+
+                    }
+                    catch (IOException e) { e.printStackTrace(); }
+
+                    final String inner_name = file_name;
+                    file_num++;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Uri uri = Uri.parse("file:///" + Environment.getExternalStorageDirectory() + "/" + inner_name);
+                            Useritem item = new Useritem( globalLine.split("@")[0],uri,"");
+                            if(globalLine.split("@")[0].equals("heewon"))
+                            {
+                                item.setResId(R.drawable.heewon);
+                            }
+                            else if(globalLine.split("@")[0].equals("sungki"))
+                            {
+                                item.setResId(R.drawable.sung);
+                            }
+                            else if(globalLine.split("@")[0].equals("jihye"))
+                            {
+                                item.setResId(R.drawable.j);
+                            }
+                            else {
+                                item.setResId(-1);
+                            }
+                            adapter.addItem(item);
+                            listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+                            listView.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+                            listView.setSelection(adapter.getCount() - 1);
+                        }
+                    });
+
+                }
+                else if (globalLine.split("@")[1].equals("normal_chatting"))
+                { //
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Useritem item = new Useritem( globalLine.split("@")[0],null,globalLine.split("@")[2]);
+                            if(globalLine.split("@")[0].equals("heewon"))
+                            {
+                                item.setResId(R.drawable.heewon);
+                            }
+                            else if(globalLine.split("@")[0].equals("sungki"))
+                            {
+                                item.setResId(R.drawable.sung);
+                            }
+                            else if(globalLine.split("@")[0].equals("jihye"))
+                            {
+                                item.setResId(R.drawable.j);
+                            }
+                            else {
+                                item.setResId(-1);
+                            }
+
+                            adapter.addItem(item);
+                            listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+                            listView.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+                            listView.setSelection(adapter.getCount() - 1);
+
+
+                        }
+                    });
+                }
+                else if (globalLine.split("@")[1].equals("hangeul_result"))
+                { //
+                    if(globalLine.split("@")[0].equals(myNickName))
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                final ArrayList<String> array = new ArrayList<String>();
+                                String s = globalLine.split("@")[2];
+                                array.add(hangeul_list[Integer.parseInt(s.split("#")[0])]);
+                                array.add(hangeul_list[Integer.parseInt(s.split("#")[1])]);
+                                array.add(hangeul_list[Integer.parseInt(s.split("#")[2])]);
+                                array.add(hangeul_list[Integer.parseInt(s.split("#")[3])]);
+                                array.add(hangeul_list[Integer.parseInt(s.split("#")[4])]);
+
+                                final String[] items = new String[5];
+
+                                for (int i = 0; i < 5; i++) {
+                                    items[i] = array.get(i);
+                                }
+
+                                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Chatting.this);
+                                alertDialogBuilder.setTitle("원하시는 글자를 선택하세요.");
+                                alertDialogBuilder.setItems(items, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        //Toast.makeText(getApplicationContext(), items[id] + " 선택했습니다.", Toast.LENGTH_SHORT).show();
+                                        editText.setText(editText.getText() + items[id]);
+                                    }
+                                });
+                                AlertDialog alertDialog2 = alertDialogBuilder.create();
+                                alertDialog2.show();
+                            }
+                        });
+
+                    }
+                }
+            }
+        }
+    });
+
+
+    public void setSocket(String ip, int port) throws IOException {
+
+        try {
+            socket = new Socket(ip, port);
+            networkWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            networkReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            MyGlobals.getInstance().setSocket(socket);
+            MyGlobals.getInstance().setNetworkReader(networkReader);
+            MyGlobals.getInstance().setNetworkWriter(networkWriter);
+
+        } catch (IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+
+    }
+
     public class ChattingAdapter extends BaseAdapter {
         ArrayList<Useritem> items = new ArrayList<Useritem>();
 
